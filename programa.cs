dd35c4544721e7296ee72869f7a19de1ccf7a107
Gerimantas <gerimantaspocius@gmail.com>
2025-04-30 19:56:41 +0300

    4 uzduotis

--- programa.cs
+++ programa.cs
@@ -5,6 +5,7 @@ using System.IO;
 
 class Programa
 {
+
     public class Naudotojas
     {
         public string Vardas { get; set; }
@@ -12,14 +13,54 @@ class Programa
         public string Miestas { get; set; }
     }
 
+    public class Administratorius : Naudotojas
+    {
+        public string Leidimai { get; set; }
+    }
+
+    public class PaprastasNaudotojas : Naudotojas
+    {
+        public string Prenumerata { get; set; }
+    }
+
+    public class BendraNaudotojuStruktura : Naudotojas
+    {
+        public string Tipas { get; set; }
+        public string Leidimai { get; set; }
+        public string Prenumerata { get; set; }
+    }
+
     static void Main(string[] args)
     {
-        string json = File.ReadAllText("naudotojai.json");
-        List<Naudotojas> naudotojai = JsonConvert.DeserializeObject<List<Naudotojas>>(json);
+        string json = File.ReadAllText("naudotojuTipai.json");
+        List<BendraNaudotojuStruktura> sarasas = JsonConvert.DeserializeObject<List<BendraNaudotojuStruktura>>(json);
 
-        foreach (var n in naudotojai)
+        foreach (var naudotojas in sarasas)
         {
-            Console.WriteLine($"Vardas: {n.Vardas}, Amžius: {n.Amžius}, Miestas: {n.Miestas}");
+            if (naudotojas.Tipas == "Admin")
+            {
+                var admin = new Administratorius
+                {
+                    Vardas = naudotojas.Vardas,
+                    Amžius = naudotojas.Amžius,
+                    Miestas = naudotojas.Miestas,
+                    Leidimai = naudotojas.Leidimai
+                };
+
+                Console.WriteLine($"ADMIN | {admin.Vardas} – Leidimai: {admin.Leidimai}");
+            }
+            else
+            {
+                var vartotojas = new PaprastasNaudotojas
+                {
+                    Vardas = naudotojas.Vardas,
+                    Amžius = naudotojas.Amžius,
+                    Miestas = naudotojas.Miestas,
+                    Prenumerata = naudotojas.Prenumerata
+                };
+
+                Console.WriteLine($"VARTOTOJAS | {vartotojas.Vardas} – Prenumerata: {vartotojas.Prenumerata}");
+            }
         }
     }
 }
